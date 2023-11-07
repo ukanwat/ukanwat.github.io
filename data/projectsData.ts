91aead891bd8a0b4cf8efd6de8a89e45ba244918
ukanwat <utkarshkanwat@gmail.com>
2023-11-07 16:16:12 +0530

    Initial commit

---
+++ data/projectsData.ts
@@ -0,0 +1,10 @@
+const projectsData = [
+  {
+    title: 'Conduit',
+    description: `Tooling Framework for LLMs`,
+    imgSrc: '/static/images/google.png',
+    href: 'https://www.example.com',
+  }
+]
+
+export default projectsData
